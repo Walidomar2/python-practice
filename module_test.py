11f0b328f6bf80706a41c3e6c4594853dafae531
Walid Omar <walidomar291@gmail.com>
2023-11-06 19:35:34 +0200

    modules

---
+++ module_test.py
@@ -0,0 +1,14 @@
+
+# you must append the module path in sys path to make the module file visiable to your App code
+import sys
+sys.path.append(r"D:\courses\Python\codes")
+
+
+def sayHello(name):
+    print(f"Hello {name}")
+
+def sayHowAreYou(name):
+    print(f"How are you {name}")
+
+
+
---
+++ modules.py
@@ -0,0 +1,29 @@
+#########################################################
+# Modules  ==> like library in C 
+
+# 1) Module is a file contain a set of functions
+# 2) You can Import module in your App to help you 
+# 3) You can Import Multiple Modules
+# 4) You Can Create Your Own Modules
+# 5) Modules Saves Your Time
+#########################################################
+
+
+""" import random
+
+print(f"Random float number: {random.random()}") """
+
+# To show all function inside module
+#print(dir(random))
+
+# to import just function or n fumction
+""" 
+from random import randint , random
+print(f"{randint(10,100)}")
+print(f"Random float number: {random()}")
+ """
+
+import module_test
+
+module_test.sayHello("Walid")
+
