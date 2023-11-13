7c4e5d764d3df87ac2cb3356cc99b2f0f8e576b9
Walid Omar <walidomar291@gmail.com>
2023-11-13 18:09:52 +0200

    OOP test1

---
+++ OOP/oop_test1.py
@@ -0,0 +1,49 @@
+#######################################################################
+# We have 3 types of methods 
+# 1) Instance Method ==> it takes at least 1 parameter (self)
+# 2) class Method    ==> we must write @classmethod above it and it takes one parameter at least (cls)
+# 3) static Method   ==> we must write @staticmethod above it and it can takes no parameters 
+
+
+class Book:
+    booksNumber = 0
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+        Book.booksNumber +=1
+        
+    def __repr__(self):
+        return f"Book: {self.title}, Author: {self.author}, Price: {self.price}"
+    
+    def getBookPrice(self):
+        return f"{self.title} price= {self.price}"
+    
+    @classmethod
+    def getBooksNumbers(cls):
+        return f"We have {cls.booksNumber} in the library."
+    
+    @staticmethod
+    def welcomeClass():
+        return "Welcome to our library"
+        
+
+
+
+
+book1 = Book('Book 1', 'Author 1', 120)
+book2 = Book('Book 2', 'Author 2', 220)
+book3 = Book('Book 3', 'Author 3', 320)
+
+#print(book1.getBooksNumbers())
+print(Book.welcomeClass())
+
+print(book1)
+print(book2)
+print(book3)
+
+print(book1.getBookPrice())
+print(Book.getBooksNumbers())
+
+book3 = Book('Book 4', 'Author 4', 420)
+print(Book.getBooksNumbers())
