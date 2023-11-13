cd754e1002d4e1c6243019aac7092ac43e227eb5
Walid Omar <walidomar291@gmail.com>
2023-11-13 22:10:13 +0200

    inheritence

--- OOP/oop_test1.py
+++ OOP/oop_test1.py
@@ -1,8 +1,11 @@
-#######################################################################
-# We have 3 types of methods 
+############################################################################################################
+# We have 3 types of methods: 
+
 # 1) Instance Method ==> it takes at least 1 parameter (self)
 # 2) class Method    ==> we must write @classmethod above it and it takes one parameter at least (cls)
-# 3) static Method   ==> we must write @staticmethod above it and it can takes no parameters 
+# 3) static Method   ==> we must write @staticmethod above it and it can takes no parameters
+ 
+############################################################################################################
 
 
 class Book:
@@ -26,11 +29,19 @@ class Book:
     @staticmethod
     def welcomeClass():
         return "Welcome to our library"
+    
+
+
+# Inhertience
+class Novel(Book):
+    
+    def __init__(self,title, author, price):
+        super().__init__(title, author, price)
         
-
-
-
-
+    #Override
+    def __repr__(self):
+        return f"Novel : {self.title}, Author: {self.author}, Price: {self.price}"
+    
 book1 = Book('Book 1', 'Author 1', 120)
 book2 = Book('Book 2', 'Author 2', 220)
 book3 = Book('Book 3', 'Author 3', 320)
@@ -46,4 +57,10 @@ print(book1.getBookPrice())
 print(Book.getBooksNumbers())
 
 book3 = Book('Book 4', 'Author 4', 420)
+print(Book.getBooksNumbers())
+
+novel1 = Novel('Novel1','author1',200)
+
+print(novel1)
+
 print(Book.getBooksNumbers())
