5b5cc5ac376eaf4daaed6b53bc07b28796fb1b78
Walid Omar <walidomar291@gmail.com>
2023-11-14 06:07:27 +0200

    word replacement function

---
+++ 20 bootCamp projects/2-word replacement.py
@@ -0,0 +1,16 @@
+
+str = input("Enter your string: ").strip()
+
+def wordReplacement(sentence):
+    
+    wordToReplace = input("Enter the word you want to replace: ").strip()
+    
+    word = input("Enter the replacement word: ").strip()
+    
+    print(f" the new sentence is : {sentence.replace(wordToReplace, word)}")
+    
+
+wordReplacement(str)
+
+    
+    
